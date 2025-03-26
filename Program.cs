da543d6b8773a4d8f5dc884ca5e8b9621be6f70d
Andreh Marques <andrehmarquez@gmail.com>
2025-03-26 16:26:25 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string frase, fraseL;
+
+Console.WriteLine("Digite uma frase para ser dita pelo cebolinha: ");
+frase = Console.ReadLine()!;
+
+
+fraseL = frase.Replace("R","L").Replace("r", "l");
+Console.Write($"Cebolinha disse: {fraseL}");
